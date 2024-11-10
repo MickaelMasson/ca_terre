5dd6ad2b0b4d124cf328bffa49623ef402b3d444
MickaelMasson <masson.mickael@me.com>
2024-11-10 17:21:58 +0100

    résolution de l'excercice terre02

--- terre02.py
+++ terre02.py
@@ -1,8 +1,20 @@
-# 
+# Afficheur d’arguments
 """
+Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
 
+
+Exemples d’utilisation :
+$> ruby exo.rb je suis solide !
+je
+suis
+solide
+!
 """
+arguments = input("écrivez : je suis solide ! ")
+liste_des_arguments = arguments.split(" ")
 
+for i in liste_des_arguments :
+    print(i)
 
 
 
@@ -10,9 +22,9 @@
 
 ### Note : 
 """
-XXmin pour faire cet exercice. 
+12min pour faire cet exercice. 
 Principal difficulté: 
-- 
+- j'utilisais mal split, ma première tentative : " ".split(variable)
 Ce que j'ai appris : 
-- 
+- variable.split(" ")
 """
