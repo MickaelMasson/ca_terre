6733a2f008957149ed95ebd1e592de55fe473cac
MickaelMasson <masson.mickael@me.com>
2024-11-10 17:02:27 +0100

    résolution de l'exercice terre01

--- terre01.py
+++ terre01.py
@@ -3,16 +3,19 @@
 
 """
 
+import os
 
+nom_fichier = os.path.basename(__file__)
+print(nom_fichier)
 
 
 
 
 ### Note : 
 """
-XXmin pour faire cet exercice. 
+15min pour faire cet exercice. 
 Principal difficulté: 
-- 
+- comprendre le import os
 Ce que j'ai appris : 
-- 
+- que l'on peut importer des modules pour augmenter les "possibilités"
 """
