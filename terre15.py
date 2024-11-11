e1956f99073e9c8e4280d07e1f53551701fdedd1
MickaelMasson <masson.mickael@me.com>
2024-11-12 00:30:24 +0100

    résolution de l'exercice terre15

--- terre15.py
+++ terre15.py
@@ -1,9 +1,17 @@
-# 
+# Terre : ok
+"""
+Créez un programme qui célèbre votre victoire.
+
+Exemples d’utilisation :
+$> ruby exo.rb
+J’ai terminé l’Épreuve de la Terre et c’était [].
+$>
+
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+
 """
 
-"""
-
-
+print("J’ai terminé l’Épreuve de la Terre et c’était interessant.")
 
 
 
