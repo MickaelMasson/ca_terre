6fa6e2c44f841c1e477560d51a7b264f24008bd5
MickaelMasson <masson.mickael@me.com>
2024-11-17 17:44:07 +0100

    Nom du programme

--- terre01.py
+++ terre01.py
@@ -12,16 +12,29 @@ crevette.js
 
 """
 
+import sys
+
+path_file = sys.argv[0]
+path_array = path_file.split("/")
+file_name = path_array[-1]
+
+print(file_name)
+
+#la même chose sur une ligne.
+print((sys.argv[0]).split("/")[-1])
+
+
+"""
+
+1ere version vu par Romain le 14 11 24
+
 import os
 
 nom_fichier = os.path.basename(__file__)
 print(nom_fichier)
 
-
-
-
 ### Note : 
-"""
+
 15min pour faire cet exercice. 
 Principal difficulté: 
 - comprendre le import os
