7c9cf7797974cd240dc4d0668f41c881e83a7df5
MickaelMasson <masson.mickael@me.com>
2024-11-18 11:11:04 +0100

    24 to 12

--- terre11.py
+++ terre11.py
@@ -9,10 +9,75 @@ $> ruby exo.rb 23:40
 Attention : midi et minuit.
 
 """
+import sys
+
+arguments = sys.argv[1:]
+
+error = "Vous devez saisir l'heure au format HH:MM"
+if len(arguments) != 1 :
+    print(f"{error} erreur 1")
+    sys.exit()
+
+argument = arguments[0]
+
+colon_index = argument.find(":")
+
+if colon_index == -1 :
+    print(f"{error} erreur 2")
+    sys.exit()
+
+if len(argument) != 5 :
+    print(f"{error} erreur 3")
+    sys.exit()
+
+hour = argument[:colon_index]
+
+if not hour.isdigit() :
+    print(f"{error} erreur 4")
+    sys.exit()
+
+hour = int(hour)
+
+if not 0 <= hour < 24 :
+    print(f"{error} erreur 5")
+    sys.exit()
+
+minute = argument[colon_index + 1:]
+
+if not minute.isdigit() :
+    print(f"{error} erreur 6")
+    sys.exit()
+
+minute = int(minute)
+
+if not 0 <= minute < 60 :
+    print(f"{error} erreur 7")
+    sys.exit()
+
+minute = str(minute).zfill(2)
+
+if hour < 12 :
+    if hour == 0 :
+        print(f"12:{minute}AM")
+        sys.exit()
+    else :
+        print(f"{hour}:{minute}AM")
+        sys.exit()
+
+if hour >= 12 :
+    if hour == 12 :
+        print(f"12:{minute}PM")
+        sys.exit()
+    else :
+        hour = hour - 12
+        print(f"{hour}:{minute}PM")
+        sys.exit()
+
+"""
 heure24 = input()
-liste_h_m = heure24.split(":")
-heure = liste_h_m[0]
-minute = liste_h_m[1]
+period = heure24.split(":")
+heure = period[0]
+minute = period[1]
 
 if heure24 == "00:00" :
     print("12:00AM")
@@ -28,7 +93,7 @@ elif int(heure) > 12 and int(heure) < 24 :
     print(f"{(int(heure) - 12)}:{minute}PM")
 
 ### Note : 
-"""
+
 25min pour faire cet exercice. 
 Principal difficulté: 
 - 
