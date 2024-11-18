03f4882ebfc4b0d348380d7da8e9aa66832dda38
MickaelMasson <masson.mickael@me.com>
2024-11-18 19:55:30 +0100

    12 to 24 / Correction, ajout de la fonction .upper() pour éviter de vérifier les valeurs en min puis maj

--- terre12.py
+++ terre12.py
@@ -30,9 +30,11 @@ if not re.match(pattern, argument):
 colon_index = argument.find(":")
 hour = argument[0:colon_index]
 minute = argument[colon_index + 1 : colon_index + 3]
-period = argument[colon_index + 3 : colon_index + 5]
+period = (argument[colon_index + 3 : colon_index + 5]).upper()
 
-if period == "AM" or period == "am":
+print(period)
+
+if period == "AM" :
     if hour == "12" :
         if minute == "00" :
             print("minuit")
@@ -44,7 +46,7 @@ if period == "AM" or period == "am":
         print(f"{hour}:{minute}")
         sys.exit()
 
-if period == "PM" or period == "pm":
+if period == "PM" :
     if hour == "12" :
         if minute == "00" :
             print("midi")
@@ -56,36 +58,3 @@ if period == "PM" or period == "pm":
         hour = int(hour)
         print(f"{(hour + 12)}:{minute}")
         sys.exit()
-
-
-
-"""
-heure12 = input()
-liste_h_m = heure12.split(":")
-heure = liste_h_m[0]
-minute_lettres = liste_h_m[1]
-minute = minute_lettres[0:2]
-lettres = minute_lettres[2:]
-
-if heure12 == "12:00AM" :
-    print("00:00 - minuit")
-elif heure12 == "12:00PM" :
-    print("12:00 - midi")
-elif heure == "12" and lettres == "AM":
-    print(f"00:{minute}")
-elif (int(heure) > 0 and int(heure) < 12) and lettres == "AM":
-    print(f"{heure}:{minute}")
-elif heure == "12" and lettres == "PM":
-    print(f"13:{minute}")
-elif (int(heure) > 0 and int(heure) < 12) and lettres == "PM"  :
-    print(f"{(int(heure) + 12)}:{minute}")
-
-
-
-### Note : 
-15min pour faire cet exercice. 
-Principal difficulté: 
-- 
-Ce que j'ai appris : 
-- 
-"""
