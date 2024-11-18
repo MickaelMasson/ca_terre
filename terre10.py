42ddb540cb162e8f0bdeaf12222bf2a0aafc191d
MickaelMasson <masson.mickael@me.com>
2024-11-18 19:46:42 +0100

    Nombre premier / correction, i pour les variable index et traduction

--- terre10.py
+++ terre10.py
@@ -1,13 +1,13 @@
 # Nombre premier
 """
-Créez un programme qui affiche si un nombre est premier ou pas.
+Créez un programme qui affiche si un number est premier ou pas.
 
 Exemples d’utilisation :
 $> node exo.js 5
-Oui, 5 est un nombre premier.
+Oui, 5 est un number premier.
 
 $> node exo.js 6
-Non, 6 n’est pas un nombre premier.
+Non, 6 n’est pas un number premier.
 
 Attention : 0 et 1 ne sont pas des nombres premiers. Gérez les erreurs d’arguments.
 
@@ -20,46 +20,21 @@ if len(arguments) != 1 :
     print("erreur, vous devez saisir un seul argument")
     sys.exit()
 
-nombre = arguments[0]
+argument = arguments[0]
 
-if not nombre.isdigit() :
+if not argument.isdigit() :
     print("Vous devez saisir un nombre entier positif")
     sys.exit()
 
-nombre = int(nombre)
+number = int(argument)
 
-if nombre < 2 :
+if number < 2 :
     print("0 et 1 ne sont pas des nombres premiers")
     sys.exit()
 
-for divider in range(2, nombre):
-    if nombre % divider == 0 :
-        print(f"Non, {nombre} n'est pas un nombre premier")
+for i in range(2, number):
+    if number % i == 0 :
+        print(f"Non, {number} n'est pas un nombre premier")
         sys.exit()
 
-print(f"Oui, {nombre} est un nombre premier.")
-
-"""
-argument = input()
-
-if int(argument) > 1 :
-    for i in range(2, int(argument) - 1) :
-        resultat_division = int(argument) / i 
-        if int(resultat_division) * i == int(argument) :
-            print(f"Non, {argument} n'est pas un nombre premier.")
-            break
-    else :
-        print(f"Oui, {argument} est un nombre premier.")
-else :
-    print("Vous devez entrer un nombre positif superieur à 1")
-
-
-### Note : 
-
-45min pour faire cet exercice. 
-Principal difficulté: 
-- dans range() ne pas commencer par la valeur 0 car erreur à la 
-  premiere division et non plus avec le 1 sinon faux positif.
-Ce que j'ai appris : 
-- l'option dans range pour définir du départ
-"""
+print(f"Oui, {number} est un nombre premier.")
