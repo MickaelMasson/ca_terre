30e02416e1622d78346c80432f2b02ca8454a73a
MickaelMasson <masson.mickael@me.com>
2024-11-18 12:33:20 +0100

    12 to 24

--- terre12.py
+++ terre12.py
@@ -10,7 +10,56 @@ $> ruby exo.rb 11:40PM
 Attention : midi et minuit.
 
 """
+import sys
+import re
 
+arguments = sys.argv[1:]
+
+if len(arguments) != 1 :
+    print("Erreur, vous devez saisir que un argument")
+    sys.exit()
+
+argument = arguments[0]
+
+pattern = r"^(?:[1-9]|[01][0-3]):[0-5]\d(?:AM|am|PM|pm)$"
+
+if not re.match(pattern, argument):
+    print(f"{argument} n'est pas un format valide. Vous devez saisir l'heure au format hh:mmAM ou hh:mmPM")
+    sys.exit()
+
+colon_index = argument.find(":")
+hour = argument[0:colon_index]
+minute = argument[colon_index + 1 : colon_index + 3]
+period = argument[colon_index + 3 : colon_index + 5]
+
+if period == "AM" or period == "am":
+    if hour == "12" :
+        if minute == "00" :
+            print("minuit")
+            sys.exit()
+        else :
+            print(f"00:{minute}")
+            sys.exit()
+    else :
+        print(f"{hour}:{minute}")
+        sys.exit()
+
+if period == "PM" or period == "pm":
+    if hour == "12" :
+        if minute == "00" :
+            print("midi")
+            sys.exit()
+        else :
+            print(f"12:{minute}")
+            sys.exit()
+    else :
+        hour = int(hour)
+        print(f"{(hour + 12)}:{minute}")
+        sys.exit()
+
+
+
+"""
 heure12 = input()
 liste_h_m = heure12.split(":")
 heure = liste_h_m[0]
@@ -34,7 +83,6 @@ elif (int(heure) > 0 and int(heure) < 12) and lettres == "PM"  :
 
 
 ### Note : 
-"""
 15min pour faire cet exercice. 
 Principal difficulté: 
 - 
