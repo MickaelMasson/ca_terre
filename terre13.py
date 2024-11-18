4700125e561c5a6b557b4833f9925e743be1648b
MickaelMasson <masson.mickael@me.com>
2024-11-18 19:57:04 +0100

    Trouver la suisse (lol) / suppression des commentaires

--- terre13.py
+++ terre13.py
@@ -45,35 +45,3 @@ elif (first_number < second_number < third_number or
     sys.exit()
 else :
     print(third_number)
-
-
-"""
-arguments = input()
-
-liste_arguments = arguments.split(" ")
-premier_argument = liste_arguments[0]
-deuxieme_argument = liste_arguments[1]
-troisieme_argument = liste_arguments[2]
-
-
-if ((int(premier_argument) > int(deuxieme_argument) and int(premier_argument) < int(troisieme_argument)) 
-    or (int(premier_argument) < int(deuxieme_argument) and int(premier_argument) > int(troisieme_argument))):
-    print(premier_argument) 
-
-if ((int(deuxieme_argument) > int(premier_argument) and int(deuxieme_argument) < int(troisieme_argument)) 
-    or (int(deuxieme_argument) < int(premier_argument) and int(deuxieme_argument) > int(troisieme_argument))):
-    print(deuxieme_argument)
-    
-if ((int(troisieme_argument) > int(deuxieme_argument) and int(troisieme_argument) < int(premier_argument)) 
-    or (int(troisieme_argument) < int(deuxieme_argument) and int(troisieme_argument) > int(premier_argument))):
-    print(troisieme_argument)
-
-
-### Note : 
-
-15min pour faire cet exercice. 
-Principal difficulté: 
-- 
-Ce que j'ai appris : 
-- 
-"""
