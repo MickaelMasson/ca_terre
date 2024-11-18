87a66b927596599630459d0a95429be0dc57b7e4
MickaelMasson <masson.mickael@me.com>
2024-11-18 20:00:21 +0100

    Trié ou pas / correction, i pour les index

--- terre14.py
+++ terre14.py
@@ -29,42 +29,9 @@ for argument in arguments :
         print("erreur")
         sys.exit()
 
-for number in range(len(arguments) - 1) :
-    if not int(arguments[number]) < int(arguments[(number + 1)]) :
+for i in range(len(arguments) - 1) :
+    if not int(arguments[i]) < int(arguments[(i + 1)]) :
         print("Pas triée !")
         sys.exit()
-    number += 1
 
 print("Triée !")
-
-maman = "there"
-papa = "umarcel"
-
-if maman > papa :
-    print("essai")
-
-
-"""
-
-if arguments.replace(" ", "").isdigit() :
-    i = 0
-    for i in range(len(liste_arguments) - 1) : 
-        if liste_arguments[int(i)] < liste_arguments[int(i) + 1] :
-            i += 1
-    if i + 1 == len(liste_arguments) :
-        print("Triée !")
-    else :
-        print("Pas trié !")     
-else :
-    print("erreur.")
-
-
-### Note : 
-
-1h50 pour faire cet exercice. 
-Principal difficulté: 
-- comment diférencier des chiffres avec des espaces et 
-  trouver la methode de l'index pour naviguer dans la liste
-Ce que j'ai appris : 
-- j'ai compris que le .isdigit renvoie True orr False
-"""
